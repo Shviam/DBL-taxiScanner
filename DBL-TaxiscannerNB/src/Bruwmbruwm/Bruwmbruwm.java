099ba6c661212f9685c81b89d750de2a344bd65e
JurvanWinden <jur.vanwinden@gmail.com>
2016-12-06 10:20:24 +0100

    Fixed last commit

--- DBL-TaxiscannerNB/src/Bruwmbruwm/Bruwmbruwm.java
+++ DBL-TaxiscannerNB/src/Bruwmbruwm/Bruwmbruwm.java
@@ -197,51 +197,5 @@ public class Bruwmbruwm {
     
     
     
-    Stack<Integer> aStar(int source, int goal){
-        PriorityQueue<NodeDist> Q = new PriorityQueue<>();
-        //resetting the node values
-        for (Node node : nodes) {
-            node.distance = Integer.MAX_VALUE;
-            node.visited = false;
-        }
-        //setting the source distance to 0 and pushing to queue
-        nodes[source].distance = 0;
-        Q.add(new NodeDist(source, heuristic(source, goal)));
-        while(!Q.isEmpty()){
-            NodeDist nd = Q.poll();
-            int k = nd.i;
-            int d = nd.d;
-            //in case of multiple entries in queue, this node is already explored
-            if(nodes[k].visited) continue;
-            nodes[k].visited = true;
-            //if we reached the goal, stop searching
-            if(k == goal) break;
-            
-            //check every neighbour of k
-            for(int u : nodes[k].neighbours){
-                //if already explored, skip it
-                if(nodes[u].visited) 
-                    continue;
-                //update distance to source, and fscore = dis to source + heuristic to end
-                int newDis = nodes[k].distance + 1;
-                int fscore = newDis + heuristic(u, goal);
-                
-                //in case there exist a faster path to u
-                if(newDis < nodes[u].distance){
-                    //set node values and add to queue
-                    nodes[u].distance = newDis;
-                    nodes[u].parent = k;
-                    Q.add(new NodeDist(u, fscore));
-                }
-            }
-        }
-        //find the path by backtracking from the goal
-        int k = goal;
-        Stack<Integer> p = new Stack<>();
-        while(k != source){
-            p.add(k);
-            k = nodes[k].parent;
-        }
-        return p;
-    }
+    
 }
