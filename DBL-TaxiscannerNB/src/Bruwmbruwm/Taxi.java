7caa185cf195c61bedca2d348b737effcd1979cc
Shivam Rastogi <shivam.rast@gmail.com>
2016-12-06 12:08:04 +0100

    highestDegree calculates 5nodes with highest degree

--- DBL-TaxiscannerNB/src/Bruwmbruwm/Taxi.java
+++ DBL-TaxiscannerNB/src/Bruwmbruwm/Taxi.java
@@ -31,24 +31,7 @@ public class Taxi {
             return false;
         }
     }
-    public void neighbour(Node  currentNode){
-        int degree;
-        int large[] = new int[5];
-        int max = storeDegree[0];
-        for(int i=0;i<currentNode.position;i++){
-             degree = currentNode.neighbours[i];
-             storeDegree[i]=degree;           
-        }
-        for(int i=0;i<=storeDegree.length;i++){
-            if(max < large[i]){
-                max = storeDegree[i];
-                large[i] = max;
-            }
-        }
-        for(int j=0; j<=5;j++){
-            System.out.println("largest 5 degree"+large[j]);
-        }
-    }    
+      
         
     
     //Pop top of stack
